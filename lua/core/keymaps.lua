e011a87ea9618d806148b632c009e7a9fed3df47
Sajidur Rahman Shajib <sajidur.inbox@gmail.com>
2025-11-18 15:02:45 +0600

    added: line move keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -9,6 +9,16 @@ vim.api.nvim_set_keymap("n", "<leader>q", ":q<CR>", {noremap = true, silent = tr
 vim.api.nvim_set_keymap("n", "<leader>s", ":w | bw<CR>", {noremap = true, silent = true})
 
 
+-- Line move
+-- Normal mode: move current line
+vim.api.nvim_set_keymap("n", "<leader>j", ":m .+1<CR>==", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>k", ":m .-2<CR>==", { noremap = true, silent = true })
+
+-- Visual mode: move selected block
+vim.api.nvim_set_keymap("v", "<leader>j", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<leader>k", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+
+
 
 -- Use leader key for a shortcut
 vim.api.nvim_set_keymap("n", "<leader>b", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
